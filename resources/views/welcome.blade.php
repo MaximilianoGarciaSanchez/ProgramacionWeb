e6b261cdf8597880c938123073c14324b7535eb0
MaximilianoGarciaSanchez <82489665+MaximilianoGarciaSanchez@users.noreply.github.com>
2021-12-16 00:23:47 -0600

    Hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,12 @@
 
 @section('content')
 
-<style>
-    .fondo{
-        background: url("02.jpg");
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    .alto{
-        height: 100vh;
-    }
-    </style>
 <div class= "container-fluid fondo">
 <div class= "row alto align-items-center justify-content-center text-center">
     <div class= "col-md 8">
 <h1>Bienvenidos</h1>
 
-<p class= "lead">Esta pa</p>
+<p class= "lead">Aqui se registrara a las personas que tengan una deuda en esta tienda</p>
 <a href="{{ route('client.index',['id'=>1]) }}" class="btn btn-primary">Clientes</a>
 </div>
 </div>
