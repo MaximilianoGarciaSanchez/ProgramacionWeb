f3f064db3f8d7d3fa6552e65eab155921b0bfae6
MaximilianoGarciaSanchez <82489665+MaximilianoGarciaSanchez@users.noreply.github.com>
2021-12-16 00:18:44 -0600

    Hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 
-<div class= "container py-5 text-center">
-<h1>Hola mundo</h1>
+<style>
+    .fondo{
+        background: url("02.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .alto{
+        height: 100vh;
+    }
+    </style>
+<div class= "container-fluid fondo">
+<div class= "row alto align-items-center justify-content-center text-center">
+    <div class= "col-md 8">
+<h1>Bienvenidos</h1>
+
+<p class= "lead">Esta pa</p>
 <a href="{{ route('client.index',['id'=>1]) }}" class="btn btn-primary">Clientes</a>
+</div>
 </div>
     
 @endsection
